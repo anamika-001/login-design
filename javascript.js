ab2901ace586ed872587207efaf0c9b6c9988307
anamika-001 <ganamika063@gmail.com>
2020-09-29 15:11:26 +0530

    working

--- javascript.js
+++ javascript.js
@@ -1,11 +1,17 @@
-$(document).ready(function() {
- 
-  $("#owl-demo").owlCarousel({
- 
-      items : 4,
-      itemsDesktop : [1199,3],
-      itemsDesktopSmall : [979,3]
- 
-  });
- 
-});
+        $('.owl-carousel').owlCarousel({
+            margin: 10,
+            loop:false;
+            nav: true,
+            navText:["<div class='nav-btn prev-slide'></div>","<div class='nav-btn next-slide'></div>"],
+            responsive: {
+                0: {
+                    items: 1
+                },
+                600: {
+                    items: 5
+                },
+                1000: {
+                    items: 5
+                }
+            }
+        });
